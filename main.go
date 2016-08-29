ea41d21870f231568c9142205c5861d2ff0ec13c
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-08-29 13:05:24 -0400

    moving and restyling map window

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 const longForm = "Jan 2, 2006 at 3:04pm (MST)"
 
 func updateTimer(w *gc.Window) {
-	t, _ := time.Parse(longForm, "Aug 28, 2016 at 7:54pm (EST)")
+	t, _ := time.Parse(longForm, "Aug 29, 2016 at 7:54pm (EST)")
 	for _ = range time.Tick(time.Second) {
 		until := t.Sub(time.Now())
 		w.MovePrint(0, 1, fmt.Sprintf("%v until vessel decay.", until.String()))
@@ -43,26 +43,20 @@ func visorView(y int) *gc.Window {
 }
 
 func vesselMap(x int, current v.Chamber, chambers []*v.Chamber) *gc.Window {
+	vmap := vp.ParseVesselMap()
+
 	// NewWindow(lines, columns, y, x)
-	mapwin, err := gc.NewWindow(10, 40, 8, x-40)
+	mapwin, err := gc.NewWindow(len(vmap)+2, 11, 0, x-11)
 	if err != nil {
 		log.Fatal(err)
 	}
 	_, x = mapwin.MaxYX()
-	title := "Map"
 	mapwin.Box(0, 0)
-	mapwin.ColorOn(1)
-	mapwin.MovePrint(1, (x/2)-(len(title)/2), title)
-	mapwin.ColorOff(1)
-	mapwin.MoveAddChar(2, 0, gc.ACS_LTEE)
-	mapwin.HLine(2, 1, gc.ACS_HLINE, x-3)
-	mapwin.MoveAddChar(2, x-2, gc.ACS_RTEE)
-
-	vmap := vp.ParseVesselMap()
 
 	vy, err := strconv.Atoi(current.Y)
 	vx, err := strconv.Atoi(current.X)
 
+	windowPadding := 1
 	for i, row := range vmap {
 		rowFormatted := strings.Trim(fmt.Sprint(row), "[]")
 		rowFormatted = strings.Replace(rowFormatted, "0", " ", -1)
@@ -71,16 +65,18 @@ func vesselMap(x int, current v.Chamber, chambers []*v.Chamber) *gc.Window {
 		if vy == i {
 			mapwin.Move(0, 0)
 			mapwin.ClearToEOL()
-			mapwin.MovePrint(4+i, 1, rowFormatted)
+			mapwin.MovePrint(windowPadding+i, 1, rowFormatted)
 
+			mapwin.ColorOn(1)
 			marker := "x"
 			if vx >= 1 {
-				mapwin.MovePrint(4+i, 1+vx*2, marker)
+				mapwin.MovePrint(windowPadding+i, 1+vx*2, marker)
 			} else {
-				mapwin.MovePrint(4+i, 1+vx, marker)
+				mapwin.MovePrint(windowPadding+i, 1+vx, marker)
 			}
+			mapwin.ColorOff(1)
 		} else {
-			mapwin.MovePrint(4+i, 1, rowFormatted)
+			mapwin.MovePrint(windowPadding+i, 1, rowFormatted)
 		}
 	}
 
@@ -102,7 +98,7 @@ func chamberMenu(stdscr *gc.Window, chamber v.Chamber, chambers []*v.Chamber) (*
 
 	_, x := stdscr.MaxYX()
 	// NewWindow(lines, columns, y, x)
-	menuwin, err := gc.NewWindow(8, 40, 0, x-40)
+	menuwin, err := gc.NewWindow(8, 40, 0, x-50)
 	if err != nil {
 		log.Fatal(err)
 	}
